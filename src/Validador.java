7737153fbb8cc653667f13d13cbf331b87225554
unibe-guipin <gpinales@unibe.edu.do>
2015-06-24 20:31:37 -0400

    Creacion de validaciones: -Valor de entrada -Cuadras -Orden

--- src/Validador.java
+++ src/Validador.java
@@ -2,35 +2,35 @@ import javax.swing.*;
 
 public class Validador{
 
-    private int mtrz1v;
-    private int mtrz2h;
-    private int mtrz2v;
-    private int mtrz1h;
+    private int mth1;
+    private int mtv1;
+    private int mth2;
+    private int mtv2;
 
-    public Validador(int mtrz1h, int mtrz1v, int mtrz2h, int mtrz2v) {
-        this.mtrz1h = mtrz1h;
-        this.mtrz1v = mtrz1v;
-        this.mtrz2h = mtrz2h;
-        this.mtrz2v = mtrz2v;
-
-        ValidaEntrada(mtrz1h, mtrz1v, mtrz2h, mtrz2v);
-    }
-
-    public static boolean ValidaEntrada(int mtrz1h, int mtrz1v, int mtrz2h, int mtrz2v) {
-        if (
-                mtrz1h >=2 &&
-                mtrz1h <= 6 &&
-                mtrz1v >= 2 &&
-                mtrz1v <= 6 &&
-                mtrz2h >= 2 &&
-                mtrz2h <= 6 &&
-                mtrz2v >= 2 &&
-                mtrz2v <= 6) {
-            /*devolver true test*/
+    public static boolean Entrada(int numero) {
+        if (numero >=2 && numero <= 6){
+            return true;
         }
-        /*devolver false*/
+        return false;
     }
 
+    public boolean Cuadradas(int mtrzh, int mtrzv) {
+        if (mtrzh == mtrzv) {
+            return true;
+        }
+        return false;
+    }
 
+    public  boolean Orden (int mh1, int mh2, int mv1, int mv2){
+        this.mth1 = mh1;
+        this.mtv1 = mv1;
+        this.mth2 = mh2;
+        this.mtv2 = mv2;
+
+        if (mh1 == mh2 && mv1 && mv2){
+            return true;
+        }
+        return false;
+    }
 
 }

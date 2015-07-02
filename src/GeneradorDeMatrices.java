95a7ac7df9d9d84fd3c3dbf126d90362e2605497
unibe-guipin <gpinales@unibe.edu.do>
2015-07-01 23:21:26 -0400

    GeneradorDeMatrices - Se inicio a trabajar la captura de las matrices.

--- src/GeneradorDeMatrices.java
+++ src/GeneradorDeMatrices.java
@@ -5,4 +5,20 @@ import javax.swing.*;
  */
 public class GeneradorDeMatrices {
 
+    private int nrows;
+    private int ncols;
+    private int [][] data;
+
+    public Matrix(int[][] dat) {
+
+        this.data = dat;
+        this.nrows = dat.length;
+        this.ncols = dat[0].length;
+    }
+
+    public Matrix(int nrow, int ncol) {
+
+        this.nrows = nrow;
+        this.ncols = ncol;
+        data = new int[nrow][ncol]; }
 }
